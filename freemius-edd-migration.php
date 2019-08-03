28c28dd46b029e780c415a926acf3c31f944a847
Vova Feldman <vova@freemius.com>
2019-08-03 12:44:52 +0300

    [plugin-version]

--- freemius-edd-migration.php
+++ freemius-edd-migration.php
@@ -3,7 +3,7 @@
      * Plugin Name: Freemius for EDD Migration
      * Plugin URI:  http://freemius.com/
      * Description: Server side endpoint to sync data between Freemius and EDD.
-     * Version:     1.0.0
+     * Version:     2.0.0
      * Author:      Freemius
      * Author URI:  http://freemius.com
      * License: GPL2
